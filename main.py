9881214168880803e09d52ccf444e07c4e538fa5
Ryan <116501522+p1poca2F3136@users.noreply.github.com>
2023-10-20 16:05:27 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+n1 = float(input("Primera nota do aluno: "))
+n2 = float(input("Segunda nota do aluno: "))
+m = (n1 + n2) / 2
+print("A media entre {:.1f} e {:.1f} é igual a {:.1F}".format(n1, n2, m))
+
+#20/10/2023
