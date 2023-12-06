b56cd6a9d46fd45601c7751190abe32f3c8d11bb
Ryan <116501522+p1poca2F3136@users.noreply.github.com>
2023-12-06 08:38:08 -0300

    Update main.py
    Atualização, adicionando um loop no programa.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-n1 = float(input("Primera nota do aluno: "))
-n2 = float(input("Segunda nota do aluno: "))
-m = (n1 + n2) / 2
-print("A media entre {:.1f} e {:.1f} é igual a {:.1F}".format(n1, n2, m))
+def calcular_media(n1, n2):
+    return (n1 + n2) / 2
+
+def main():
+    while True:
+        try:
+            n1 = float(input("Digite a primeira nota do aluno (ou 'sair' para encerrar): "))
+            
+            if n1.lower() == 'sair':
+                break
+            
+            n2 = float(input("Digite a segunda nota do aluno: "))
+
+            media = calcular_media(n1, n2)
+
+            print("A média entre {:.1f} e {:.1f} é igual a {:.1f}".format(n1, n2, media))
+        except ValueError:
+            print("Por favor, digite notas válidas.")
+
+if __name__ == "__main__":
+    main()
 
 #20/10/2023
+#(ꈍᴗꈍ)
